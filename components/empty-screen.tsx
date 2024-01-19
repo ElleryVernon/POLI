8c72a929303628d61a04a2a79ce61a36382cbcde
ElleryVernon <tmpks3@gmail.com>
2024-01-19 10:08:33 +0900

    first commit

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -6,16 +6,8 @@ import { IconArrowRight } from '@/components/ui/icons'
 
 const exampleMessages = [
   {
-    heading: '자기 소개 해줘',
-    message: `자기 소개 할 수 있어??`
-  },
-  {
-    heading: '게임 좋아해?',
-    message: '너 게임 좋아해?'
-  },
-  {
-    heading: '독도에 대해 어떻게 생각해?',
-    message: `독도에 대해 알아?`
+    heading: '내 제품에 대한 큐시트를 작성해줘.',
+    message: `내 제품은 '드러머 원형 접시 플레이트 500ml'야. 장점은 '가벼우면서도 튼튼한 내구성', '좋은 가성비', '열에 강한 특수 재질' , '모던하고 깔끔한 디자인' 이고 고객리뷰는 '신혼 그릇 세트로 추천', '스크래치에 강한 내구성' 등이 있어. 주요 특징은 '겹겹히 쌓기 좋은 형태', '모던한 디자인', '음식 온도를 오래 유지' 등이 있어.`
   }
 ]
 
@@ -23,9 +15,11 @@ export function EmptyScreen({ setInput }: Pick<UseChatHelpers, 'setInput'>) {
   return (
     <div className="mx-auto max-w-2xl px-4">
       <div className="rounded-lg border bg-background p-8">
-        <h1 className="mb-2 text-lg font-semibold">안녕! 라디안이야!</h1>
+        <h1 className="mb-2 text-lg font-semibold">
+          안녕하세요! 래빗라이브 큐시트 AI 입니다.!
+        </h1>
         <p className="mb-2 leading-normal text-muted-foreground">
-          나에게 궁금한 점이 있다면 언제든지 질문해줘!
+          도와드릴 일이 있다면 언제든지 말씀해주세요!
         </p>
         <p className="leading-normal text-muted-foreground">
           아래 같은 주제로 대화를 시작해보는건 어떄?
