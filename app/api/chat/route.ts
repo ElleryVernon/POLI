89e688aaf536026f1fa6d97c8b26c21646eedcca
ElleryVernon <tmpks3@gmail.com>
2024-03-05 13:38:12 +0900

    first commit

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -116,10 +116,10 @@ export async function POST(req: Request) {
       ...messages.slice(-4)
     ],
     model: 'gpt-4-turbo-preview',
-    temperature: 0.3,
+    temperature: 0.6,
     max_tokens: 4096,
-    frequency_penalty: 0.05,
-    presence_penalty: 0.1,
+    frequency_penalty: 0.025,
+    presence_penalty: 0.05,
     top_p: 1,
     stream: true
   })
