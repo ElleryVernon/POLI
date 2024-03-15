174ba2a6a9f0a51f9800348b8aba2c246c0f1ad9
ElleryVernon <tmpks3@gmail.com>
2024-03-15 18:59:45 +0900

    first commit

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -6,12 +6,12 @@ import { IconArrowRight } from '@/components/ui/icons'
 
 const exampleMessages = [
   {
-    heading: '요한복음 3장 16절 말씀은 어떤 내용인가요?',
-    message: `요한복음 3장 16절 말씀은 어떤 내용인가요?`
+    heading: '로맨스 스캠이 뭔가요?',
+    message: `로맨스 스캠에 대해 자세히 알려주세요.`
   },
   {
-    heading: '오늘의 말씀을 추천해주세요.',
-    message: `오늘의 말씀을 추천해주세요.`
+    heading: '사이버 범죄의 신고 절차는 어떻게 이루어지나요?',
+    message: `사이버 범죄의 신고 절차에 대해 자세히 알려주세요.`
   }
 ]
 
@@ -20,10 +20,10 @@ export function EmptyScreen({ setInput }: Pick<UseChatHelpers, 'setInput'>) {
     <div className="mx-auto max-w-2xl px-4">
       <div className="rounded-xl border bg-background p-8">
         <h1 className="mb-2 text-lg font-semibold">
-          안녕하세요! TOG.AI 입니다!
+          안녕하세요! 경찰청 사이버범죄 피해자 지원 AI 폴리입니다. 
         </h1>
         <p className="mb-2 leading-normal text-muted-foreground">
-          성경에 대해 궁금한 점이 있다면 언제든지 말씀해주세요!
+          궁금하거나 도와드릴 부분이 있다면 언제든지 말씀해주세요!
         </p>
         <p className="leading-normal text-muted-foreground">
           아래 같은 주제로 대화를 시작해보는건 어떤가요?
